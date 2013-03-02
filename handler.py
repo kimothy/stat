e921a9d72a025ba8ef9ae855d95c1e01a07a86be
kimothy <kimtengh@gmail.com>
2013-03-02 18:05:39 +0100

    first commit

--- handler.py
+++ handler.py
@@ -1,142 +1,143 @@
-import random
+""" Random data generator """
+import random as rdm
 import csv
 import numpy as np
+from optparse import OptionParser
 
-categories = ["kultur","vitenskap","natur","musikk","verden"]
-cards = 296
-cards = int(cards/5)
-cards = np.linspace(1,cards,cards)
 
-minTime = 5.0
-maxTime = 60.0
-tics = minTime
-boundaries = []
-mimeData = []
-BmimeData = []
-bnumbers = []
+""" Default Values """
+Cards = 296					# number of question cards in deck
+Min = 5.0					# minimum answer time
+Max = 60.0					# maximum answer time
+Msd = 7.0					# mime standard deviation
+Mav = 15.0					# mime avrage answer time
+Dsd = 15.0					# draw standard deviation
+Dav = 30.0					# draw avrage answer time
+Bias = 0.3					# bias. mime vs. draw
+Mean = Max / 2				# default mean value
+Deviation = Max / 4			# default deviation value
+data_file = "rawdata.csv"	# default name for data file
+Categories = ["kultur","vitenskap","natur","musikk","verden"]
+Methods = ["mime","draw"]
 
-def randomNumber(mean,deviation):
-	choice = random.randint(1,3)
-	if  choice != 3:
-		randData = random.normalvariate(mean,deviation)
-	elif choice == 3:
-		randData = random.uniform(minTime,maxTime+20)
+
+""" Option Parser """
+parser = OptionParser()
+parser.add_option("-c", dest="cards",help="number of cards in deck")
+
+parser.add_option("-i","--min", dest="min",help="minimum answer time")
+
+parser.add_option("-a","--max", dest="max",help="maximum answer time")
+
+parser.add_option("-s","--msd", dest="msd",help="standard deviation for mime data")
+
+parser.add_option("-v","--mav", dest="mav",help="mean of mime data")
+
+parser.add_option("-t","--dsd", dest="dsd", help="standard deviation for draw data")
+
+parser.add_option("-w","--dav", dest="dav",help="mean of draw data")
+
+parser.add_option("-b","--bias", dest="bias",help="mime vs. draw bias")
+ 
+parser.add_option("-f","--file", dest="data_file",help="set filename",metavar="file")
+ 
+(options, args) = parser.parse_args()
+
+""" Option handler """
+if options.cards != None:
+	Cards = options.cards
+
+if options.min != None:
+	Min = options.min
+	
+if options.max != None:
+	Max = options.max
+	
+if options.msd != None:
+	Msd = options.msd
+	
+if options.mav != None:
+	Mav != options.mav
+	
+if options.dsd != None:
+	Dsd = options.dsd
+	
+if options.dav != None:
+	Dav = options.dav
+	
+if options.bias != None:
+	Bias != options.bias
+	
+if options.data_file != None:
+	data_file = data_file
+
+print "Cards =", Cards,"Min =", Min,"Max =",Max,"Bias =", Bias
+print "Msd =", Msd,"Mav =",Mav,"Dsd =", Dsd,"Dav =",Dav
+
+""" Fuctions """
+def time(method, Bias):
+	if method == Methods[0]:
+		Mean = Mav
+		Deviation = Msd
+		Biaz = Bias * 0.5
+	elif method == Methods[1]:
+		Mean = Dav
+		Deviation = Dsd
+		Biaz = (1 - Bias) * 0.5
 	else:
-		randData = 0
-		
-	if randData < minTime or randData > maxTime: 
-		randData = 0
-	return randData
+		print "Time() error: Cant recognize method; ",method
+		Mean = Max / 2
+		Deviation = Max / 4
+		Bias = 0.25
 	
-def getMime():
-	mean = 15.0
-	deviation = 8.0
-	data = randomNumber(mean,deviation)
-	zeroBiaz = random.randint(1,3)
-	if zeroBiaz == 3:
-		data = 0
-	return data
-	
-def getDraw():
-	mean = 30.0
-	deviation = 15.0
-	data = randomNumber(mean,deviation)
-	return data
-
-def makeBound():
-	n = 0
-	while n < maxTime:
-		n += tics
-		boundaries.append(n)
-
-def getBound(mimeData):
-	for bound in boundaries:
-		if mimeData <= bound and mimeData > bound - tics:
-			mimeData = int(bound)
-			return bound		
-	return int(0)
-
-def getDifficulty(s):
-	chance = random.randint(1,6)
-	if s == 0:
-		if chance <= 3 and chance >= 1:
-			return 3
-		elif chance == 4 or chance == 5:
-			return 2
+	chance = rdm.random()	
+	if chance > Biaz:
+		if chance <= 0.5 and chance >= 0:
+			return rdm.normalvariate(Mean,Deviation)
+		elif chance > 0.5 and chance <= 1:
+			return rdm.uniform(Min,Max+20)
 		else:
-			return 1
+			print "Time() error: chance error; ",chance
+			return Max / 2
+	elif chance < Biaz:
+		return 0
 	else:
-		if chance >= 1 and chance <= 3:
-			return 1
-		elif chance == 4 or chance == 5:
-			return 2
-		else:
-			return 3		
-			
-def getRandomData()	:
-	raw_file = open("raw_data.csv","wb")
-	raw = csv.writer(raw_file)
-	raw.writerow(["Time","Bound Time", "Type", "Category", "Difficulty"])
+		print "Time() error: Biaz error; ",Biaz
+		return 0
 	
-	for card in cards:
-		for category in categories:
-			if card % 2 != 0:
-				Type = "mime"
-				t = getMime()
-			else:
-				Type = "draw"
-				t = getDraw()
+def difficulty(t):
+	chance = rdm.random()
+	if chance <= 0.5 and t != 0 or chance >= 0.8 and t == 0:
+		return 1
+	elif chance > 0.5 and chance < 0.8:
+		return 2
+	elif chance >= 0.8 and t != 0 or chance <= 0.5 and t == 0:
+		return 3
+	else:
+		print "difficulty() error: chance error; ",chance
+		return 2
 	
-			s = int(getBound(t))
-			diff = getDifficulty(s)
-			raw.writerow([t,s,Type,category,diff])
-	raw_file.close()
 
-makeBound()
-getRandomData()
-
-raw_file = open("raw_data.csv","r")
-raw = csv.reader(raw_file)
-
-
-mimeValue = []
-drawValue = []
-dataType = []
-
-for row in raw:
-	try:
-		value = int(row[1])
-		if str(row[2]) == "mime":
-			mimeValue.append(value)
-		elif str(row[2]) == "draw":
-			drawValue.append(value)
-		
-	except:
-		print "Error: not integer; "+str(row[1])
-		
-raw_file.close()
-data_file = open("data.csv","wb")
+""" Main Program """
+data_file = open(data_file,"wb")
 data = csv.writer(data_file)
+data.writerow(["Method","Category","Difficulty","Time"])
 
-data.writerow(["mime","data"])
-for bound in boundaries:
-	count = mimeValue.count(bound)
-	data.writerow([bound,count])
-	
-data.writerow(["draw","data"])
-for bound in boundaries:
-	count = drawValue.count(bound)
-	data.writerow([bound,count])
-	
-data.writerow(["both","data"])
-for bound in boundaries:
-	count = mimeValue.count(bound)+drawValue.count(bound)
-	data.writerow([bound,count])
-	
-	
+n = 0
+while n < Cards:
+	for category in Categories:
+		for method in Methods:
+			n += 1
+			if n < Cards:
+				t = time(method, Bias)
+				if t < 0 or t > Max:
+					t = 0
+				d = difficulty(t)
+				data.writerow([method,category,d,t])
+				
+			
+			
 
-	
-	
 
 
 
