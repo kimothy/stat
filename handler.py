88e7da43e632b11870174896743366c581d07a61
Kim Timothy Engh <kimothy@kimothy-desktop.(none)>
2013-03-03 20:28:17 +0100

    updates

--- handler.py
+++ handler.py
@@ -1,25 +1,27 @@
 """ Data handler """
 import csv
 from optparse import OptionParser
-
+import sys
 
 """ Default Values """
 tics = 10.0
 method = ["mime","draw"]
 file_name = "rawdata.csv"	# default name for data file
 Max = 0
-Check = False
+Bounderies = []
 
 """ Option Parser """
 parser = OptionParser()
  
-parser.add_option("-f","--input", dest="file_name",help="data filename",metavar="file")
+parser.add_option("-r","--read", dest="file_name",help="data filename",metavar="file")
 
-parser.add_option("-s","--output", dest="save_file",help="save filename",metavar="file")
+parser.add_option("-s","--save", dest="save_file",help="save filename",metavar="file")
+
+parser.add_option("-i","--tics",dest="tics", help = "Sets data tics", metavar="tics")
 
 parser.add_option("-t","--task",dest="task", help = "Task: Valid options; 'method'", metavar="task")
 
-parser.add_option("-i","--tics",dest="tics", help = "Sets data tics", metavar="tics")
+parser.add_option("-p","--plot",dest="plot", help = "Plot", metavar="True")
 
 (options, args) = parser.parse_args()
 
@@ -28,6 +30,9 @@ if options.file_name != None:
 	file_name = options.data_file
 if options.task != None:
 	task = options.task
+else:
+	sys.exit("ERROR; no task selected")
+	
 if options.tics != None:
 	tics = int(options.tics)
 if options.save_file != None:
@@ -35,11 +40,26 @@ if options.save_file != None:
 else:
 	save_file = str(options.task)+"_output.cvs"
 	
-print "Opening",file_name
-print "Saving to", save_file
+print "Opening %s, and saving to %s" %(file_name,save_file)
 
 """ Functions """
+def points():
+	data_file = open(file_name,"r")
+	data = csv.reader(data_file)
+	points = -1
+	data = csv.reader(data_file)
+	for row in data:
+		points += 1	
+	return points
+	
+def bounderies():
+	for x in xrange(int(tics),int(Max+tics),int(tics)):
+		Bounderies.append(x)
+	print "Bouderies:",Bounderies
+	
 def maxValue():
+	data_file = open(file_name,"r")
+	data = csv.reader(data_file)
 	X = 0
 	for row in data:
 		try:
@@ -51,63 +71,68 @@ def maxValue():
 			print "ERROR: Faild to read file;", file_name
 	print "Maximum value:",X
 	return X
-
-def getBounderies():
-	bounderies = []
-	n = 0
-	while n < Max:
-		n += tics
-		bounderies.append(int(n))
-	return bounderies
 	
 def methodStats(method):
-	n = 0
-	
 	save.writerow(["methodStats",method])
-	for tic in bounderies:
-		for row in data:
-			if row[0] == method or row[0] == "both" and row[3] > tic-tics and row [3] < tic:
-				n += 1
-				
-		save.writerow([tic,n])
-				
+	n = 0
+	m = 0
+	y = 0
+	while m < Max:
+		x = 0
+		n = m
+		m += tics
+		data_file = open(file_name,"r")
+		data = csv.reader(data_file)
+		for item in data:
+			try:
+				if float(item[3]) > float(n) and float(item[3]) < float(m) and (item[0] == method or "both" == method):
+					x += 1
+			except:
+				if item[3] != "Time":
+					print "methodStats() ERROR: could not convert to float;",item[3]
+		y += x
+		save.writerow([int(n),int(m),x])
+	print "%s answerd successfully %d times" %(method.upper(),y)
 
 """ Main Program """
 data_file = open(file_name,"r")
 data = csv.reader(data_file)
 
+points = points()
+print "Number of data points: %d" %(points)
+
 savefile = open(save_file,"wb")
 save = csv.writer(savefile)
-	
+
 if options.task == "method":
 	Max = maxValue()
-	bounderies = getBounderies()
+	bounderies()
 	methodStats("both")
 	methodStats("mime")
 	methodStats("draw")
 	
-
-
-
-
 				
-			
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+				
+				
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
