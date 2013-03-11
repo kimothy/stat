e86af6b2d9ef8f7847df2954f8a3fdded04c20ec
kimothy <kimtengh@gmail.com>
2013-03-11 20:44:01 +0100

    update

--- handler.py
+++ handler.py
@@ -1,146 +1,72 @@
-""" Data handler """
-import csv
+#! /usr/bin/python3
 from optparse import OptionParser
-import sys
-from pylab import *
-import matplotlib.pyplot as plt
+import csv
+from math import *
 
-""" Default Values """
-tics = 10.0
-method = ["mime","draw"]
-file_name = "rawdata.csv"	# default name for data file
-Max = 0
-Bounderies = []
-drawValues = []
-mimeValues = []
-bothValues = []
-
-""" Option Parser """
 parser = OptionParser()
- 
-parser.add_option("-r","--read", dest="file_name",help="data filename",metavar="file")
-
-parser.add_option("-s","--save", dest="save_file",help="save filename",metavar="file")
-
-parser.add_option("-i","--tics",dest="tics", help = "Sets data tics", metavar="tics")
-
-parser.add_option("-t","--task",dest="task", help = "Task: Valid options; 'method'", metavar="task")
-
-parser.add_option("-p","--plot",dest="plot", help = "Plot", metavar="True")
-
+parser.add_option("-p","--plot", action="store_true",dest="plot_flag",default="False")
+parser.add_option("-i","--input",action="store",type="string",dest="input_file",default="rawdata.csv")
+parser.add_option("-o","--output",action="store",type="string",dest="output_file",default="output.csv")
+parser.add_option("-w","--write",action="store_false",dest="write",default="True")
+parser.add_option("-t","--tics",action="store",type="int",dest="tics",default=10)
+parser.add_option("-m","--method",action="store_true",dest="method",default="False")
 (options, args) = parser.parse_args()
+print("Opening",options.input_file,"and saving to",options.output_file)
 
-""" Options Handler """
-if options.file_name != None:
-	file_name = options.data_file
-if options.task != None:
-	task = options.task
-else:
-	sys.exit("ERROR; no task selected")
+def openInput(column = "time"):
+	data = csv.reader(open(options.input_file,"r"),delimiter=',')
+	col = next(data).index(column)
+	return {'data':data,'col':col}
+
+def colSum(method,col = "time"):
+	inFile = openInput()
+	return sum(float(row[inFile['col']]) for row in inFile['data'])
 	
-if options.tics != None:
-	tics = int(options.tics)
-if options.save_file != None:
-	save_file = str(options.save_file)
-else:
-	save_file = str(options.task)+"_output.cvs"
+def colRows():
+	x = 0
+	for row in openInput()['data']:
+		x += 1
+	return x
+						
+def limits():
+	lim = {}
+	lim['0'] = 0
+	for x in range(options.tics,int(largest("time"))+options.tics,options.tics):
+		lim[str(x)] = 0
+	return lim
 	
-print "Opening %s, and saving to %s" %(file_name,save_file)
-
-""" Functions """
-def points():
-	data_file = open(file_name,"r")
-	data = csv.reader(data_file)
-	points = -1
-	data = csv.reader(data_file)
-	for row in data:
-		points += 1	
-	return points
+def avrage(method):
+	return colSum(method) / colRows(method)
 	
-def bounderies():
-	for x in xrange(int(tics),int(Max+tics),int(tics)):
-		Bounderies.append(x)
-	print "Bouderies:",Bounderies
+def largest(col):
+	inFile = openInput(col)
+	return max(float(row[inFile['col']]) for row in inFile['data'])
 	
-def maxValue():
-	data_file = open(file_name,"r")
-	data = csv.reader(data_file)
-	X = 0
-	for row in data:
-		try:
-			if row[3] == "Time":
-				Check = True
-			elif float(row[3]) > float(X):
-				X = float(row[3])
-		except:
-			print "ERROR: Faild to read file;", file_name
-	print "Maximum value:",X
-	return X
-
-def methodArrays():
-	save.writerow(["group","both","mime","draw"])
-	dataArray = []
-	for bound in Bounderies:
-		data_file = open(file_name,"r")
-		data = csv.reader(data_file)
-		x = [0,0]
-		for item in data:
-			try:
-				if float(item[3]) != 0 and float(item[3]) >= float(bound)-float(tics) and float(item[3]) < float(bound) and (item[0] == "mime"):
-					x[0] += 1
-				elif float(item[3]) != 0 and float(item[3]) >= float(bound)-float(tics) and float(item[3]) < float(bound) and (item[0] == "draw"):
-					x[1] +=1
-			except:
-				if item[3] != "Time":
-					print "methodArrays() ERROR: could not convert to float;",item[3]
-		drawValues.append(x[0])
-		mimeValues.append(x[1])
-		bothValues.append(x[0]+x[1])
-		save.writerow([int(bound),x[0]+x[1],x[0],x[1]])
-
-""" Main Program """
-data_file = open(file_name,"r")
-data = csv.reader(data_file)
-
-points = points()
-print "Number of data points: %d" %(points)
-
-savefile = open(save_file,"wb")
-save = csv.writer(savefile)
-
-if options.task == "method":
-	Max = maxValue()
-	bounderies()
-	dataArray = methodArrays()
-	if options.plot == "True":
-		print "Combined",bothValues
-		print "Mime Values",mimeValues
-		print "Draw Values",drawValues
-		plt.plot(Bounderies,bothValues,Bounderies,mimeValues,Bounderies,drawValues)
-		plt.show()
-		
+def sortMethod():
+	ix = {'t':openInput("time")['col'],'m':openInput("method")['col']}
+	data = {'draw':limits(),'mime':limits()}
+	for limit in limits():
+		for row in openInput()['data']:
+			if float(row[ix['t']]) >= float(limit)-options.tics and float(row[ix['t']]) < float(limit):
+					temp = (data[str(row[ix['m']])])
+					temp[str(limit)] += 1
+					data[str(row[ix['m']])] = temp
+	return data
 			
-				
-				
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+def sd(method):
+	inFile = openInput("time")
+	avr = avrage(method)
+	x = 0
+	for row in inFile["data"]:
+		x += (float(row[inFile['col']])-float(avr))**2
+		
+	x = sqrt(x / (colRows(method) -1))
+	return x
 
+if options.method == True:
+	print("Avrage:",avrage())
+	print("Standard deviation:",sd())
+
+	
+	
+	
